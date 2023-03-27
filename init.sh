fffa987a899fe91ec01a338252169e08db5cac51
dexter <daixtr@gmail.com>
2023-03-27 23:52:30 +0800

    Check symlink before create

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ X=$(echo $SCRIPTPATH | sed 's|/|\\/|g')
 sed -i "s/<<replaceme>>/$X/" $SCRIPTPATH/env.rc $SCRIPTPATH/tmux/tmux.conf $SCRIPTPATH/tmux/tload.sh $SCRIPTPATH/tmux/tsave.sh
 mkdir -p $SCRIPTPATH/tmux/buffers
 mkdir -p $SCRIPTPATH/tmux/capture-pane
-ln -sf $SCRIPTPATH/tmux/tmux.conf ~/.tmux.conf
-ln -sf $SCRIPTPATH/vifm ~/.vifm
+[ ! -r ~/.tmux.conf ] && ln -sf $SCRIPTPATH/tmux/tmux.conf ~/.tmux.conf
+[ ! -r ~/.vifm ] && ln -sf $SCRIPTPATH/vifm ~/.vifm
